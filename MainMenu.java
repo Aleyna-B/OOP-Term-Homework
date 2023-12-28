afbfb392ce4958503e7f02d66f6740a3a390211f
Aleyna-B <122743833+Aleyna-B@users.noreply.github.com>
2023-12-29 02:27:39 +0300

    second addition via upload
    attempt to update the files and the project

--- MainMenu.java
+++ MainMenu.java
@@ -1,9 +1,11 @@
 package com.mycompany.ooptermhw;
-import com.mycompany.ooptermhw.Dairy.*;
+import com.mycompany.ooptermhw.Dairy.DaysInfo;
+import com.mycompany.ooptermhw.food.ForFood;
+import com.mycompany.ooptermhw.study.Studies;
 
 public class MainMenu {
 	EntryScreen es = new EntryScreen();
-	private int controller;
+	private int controller;		//to control the program
 	public MainMenu(int controller)
 	{		
 		this.setController(controller);
@@ -22,6 +24,12 @@ public class MainMenu {
 			break;
 			
 			case 2: DaysInfo end= new DaysInfo();
+			break;
+			
+			case 3: ForFood ff = new ForFood();;
+			break;
+			
+			case 4: Studies studies = new Studies();
 			break;
 			}
 		}
---
+++ Work.java
@@ -0,0 +1,30 @@
+package com.mycompany.ooptermhw.study;
+
+import com.mycompany.ooptermhw.ISubAnswer;
+import com.mycompany.ooptermhw.InputControl;
+
+public class Work extends InputControl implements ISubAnswer{
+	public void baseAnswer()
+	{
+		System.out.println("I am not sure about favourite subjects. Definetely not abstract maths classes "
+				+ "or data structures.\nCan be OOP or EE circuits.\nDid this answer your question?\n(1)- Yes\n(2)- No");
+		baseControl();
+		if(this.getOpt()==1) System.out.println("Marvellous.\n");
+		else if(this.getOpt()==2) 
+		{
+		System.out.println("Do you know the video about english being someones favourite language?\n(1)- Yes\n(2)- No");	
+		baseControl();
+		if(this.getOpt()==1) System.out.println("Hahah\n");
+		else if(this.getOpt()==2) System.out.println("Watch this: https://youtu.be/39UDZMgPg5k?feature=shared \n");
+		}
+	}
+	
+	public void intervalControl()
+	{
+		if(this.getOpt()<1 || this.getOpt()>5)
+		{
+			System.out.println("I think you accidentally pressed something you should not have.\n"
+					+ "The options you can choose are from 1 to 5\n");
+		}
+	}
+}
